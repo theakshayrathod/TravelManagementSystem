5b7b79d2d6122ae09ede1e2f43250082662f801e
decodegaurav01 <i.gaurav002@gmail.com>
2025-07-23 00:27:11 +0530

    User and operator Update Profile done

---
+++ TMS_Frontend/src/Pages/operator/UpdateProfile.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+// import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import type { JSX } from 'react';
+
+export function OperatorProfileUpdate():JSX.Element{
+
+  return (
+    <div className='flex justify-center items-center '>
+       {/* <div className=" h-[350px] w-[70%] flex bg-gray-200 border-gray-500 rounded-md shadow-lg  "></div> */}
+    <div className="  mt-10 rounded-md shadow-lg bg-gray-100 mx-5 border-2">
+          <h2 className="text-3xl font-bold  text-gray-900 mx-35 ">Bus Operator Profile</h2>    
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label htmlFor="name" className="block text-sm/6 font-medium ">
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="-name"
+                  name="name"
+                  type="text"
+                  // autoComplete="given-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-4">
+              <label htmlFor="email" className="block text-sm/6 font-medium ">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  // autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label htmlFor="licens" className="block text-sm/6 font-medium text-gray-900">
+                Licens No
+              </label>
+              <div className="mt-2">
+                <input
+                  id="licens"
+                  name="licens"
+                  type="text"
+                  // autoComplete="street-address"
+                  className="block w-100 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div className="col-span-full">
+              <label htmlFor="licens" className="block text-sm/6 font-medium text-gray-900">
+                Address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="licens"
+                  name="licens"
+                  type="text"
+                  // autoComplete="street-address"
+                  className="block w-100 h-20 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label htmlFor="contact" className="block text-sm/6 font-medium text-gray-900">
+               Contact
+              </label>
+              <div className="mt-2">
+                <input
+                  id="contact"
+                  name="contact"
+                  type="tel"
+                  // autoComplete="address-level2"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            {/* <div className="sm:col-span-2">
+              <label htmlFor="postal-code" className="block text-sm/6 font-medium text-gray-900">
+                ZIP / Postal code
+              </label>
+              <div className="mt-2">
+                <input
+                  id="postal-code"
+                  name="postal-code"
+                  type="text"
+                  // autoComplete="postal-code"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div> */}
+          </div>
+        </div>
+        </div>
+  )
+
+}
