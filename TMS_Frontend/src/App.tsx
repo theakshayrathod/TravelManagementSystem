0ecbf4cf2468a4386934d6b6ec035f7b82548b34
jitenpatil1013 <patiljiten1013@gmail.com>
2025-07-23 19:26:50 +0530

    Bookings view  done

--- TMS_Frontend/src/App.tsx
+++ TMS_Frontend/src/App.tsx
@@ -1,19 +1,20 @@
 
 import type {JSX} from 'react'
-import { Login } from './Pages/auth/Login'
+//import { Login } from './Pages/auth/Login'
 // import { Home } from './Pages/Home'
 import  Navbar  from './components/Navbar'
+import { Bookings } from './Pages/operator/Bookings'
 // import { OperatorRegistration } from './Pages/auth/operator/Registration'
 // import { UserRegistration } from './Pages/auth/user/Registration'
 // import { UserProfileUpdate } from './Pages/user/UpdateProfile'
 
 //import { OperatorProfileUpdate } from './Pages/operator/UpdateProfile'
-import { AddTrip } from './Pages/operator/AddTrip'
+//import { AddTrip } from './Pages/operator/AddTrip'
 
 // import { OperatorProfileUpdate } from './Pages/operator/UpdateProfile'
 
 // import { Registration } from './Pages/auth/Registration'
-import {AddBus} from './Pages/operator/AddBus'
+//import {AddBus} from './Pages/operator/AddBus'
 
 
 
@@ -39,6 +40,7 @@ function App():JSX.Element {
     {/* <UserProfileUpdate/> */}
     {/* <OperatorRegistration/> */}
     {/* <UserRegistration/> */}
+    
 
 
 
