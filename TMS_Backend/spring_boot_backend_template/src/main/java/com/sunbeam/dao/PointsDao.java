2772af63824984291f96b64c6fabba74b9b0d2a6
decodegaurav01 <i.gaurav002@gmail.com>
2025-08-01 00:01:44 +0530

    pick and drop add, view all points, done

---
+++ TMS_Backend/spring_boot_backend_template/src/main/java/com/sunbeam/dao/PointsDao.java
@@ -0,0 +1,9 @@
+package com.sunbeam.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sunbeam.entity.Point;
+
+public interface PointsDao extends JpaRepository<Point, Long>{
+
+}
